a2b8e02253f7921dc708184f3a35297bd16508c6
scamilas <scamilabarros@hotmail.com>
2022-02-16 00:20:14 -0300

    exercicio finalizado

--- modulo1/strings-arrays/index.js
+++ modulo1/strings-arrays/index.js
@@ -1,38 +1,38 @@
-/*let array
-console.log('a. ', array)
 
-array = null
-console.log('b. ', array)
+//const frase = prompt("Digite uma frase")
+//console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
 
-array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-console.log('c. ', array.length)
 
-let i = 0
-console.log('d. ', array[i])
-
-array[i+1] = 19
-console.log('e. ', array)
-
-const valor = array[i+6]
-console.log('f. ', valor)
-
-const frase = prompt("Digite uma frase")
-
-console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+//exercicio1
+/*
+const nome = prompt ("digite seu nome")
+const email = prompt ("digite seu email")
+const frase = `o email ${email} do usuario foi cadastrado com sucesso. Seja bem vindo (a) ${nome}!`
+console.log(frase)
 */
 
-//EXERCICIO1
 
+//exercicio2
+/*
+const comidasFavoritas = ["macarrao sem molho", "tilapia crua", "formiga frita", "salmao cru", "tapioca sem recheio"]
+const comidasFavoritaUsuario = prompt('qual a sua comida favorita?')
+console.log (comidasFavoritas)
+console.log ("essas são as minhas comidas faviritas:", comidasFavoritas[0])
+console.log (comidasFavoritas[1])
+console.log (comidasFavoritas[2])
+console.log (comidasFavoritas[3])
+console.log (comidasFavoritas[4])
+comidasFavoritas[1] = comidasFavoritaUsuario
+console.log (comidasFavoritas)
+*/
 
-
-//EXERCICIO2
-function imprimirIdade () {
-    const anoAtual = Number(prompt("digite ano atual"))
-    const anoDeNascimento = Number(prompt("digite ano de nascimento"))
-    console.log(anoDeNascimento)
-}
-
-
-//EXERCICIO3
-
-
+//exercicio3
+/*const listaDeTarefas = []
+const tarefa = prompt ("insira uma tarefa")
+const tarefa1 = prompt ("insira outra tarefa")
+const tarefa2 = prompt ("insira mais uma tarefa")
+const digiteUmaTarefa = prompt ("qual tarefa foi realizada?")
+listaDeTarefas.push(tarefa, tarefa1, tarefa2)
+console.log (listaDeTarefas)
+listaDeTarefas.splice(Number(tarefasRealizadas),1)
+*/
